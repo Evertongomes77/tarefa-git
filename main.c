017b51fd67cd74a550a65c3a2881cc1ac15e8821
devgomes <evertongc709@gmail.com>
2026-09-12 10:50:36 -0400

    adicionando o campo de código de barras e imprimindo na tela o id e código de barras

--- main.c
+++ main.c
@@ -13,8 +13,9 @@ void exibir_menu(void) {
 void listar_produtos(Produto lista[], int total) {
     printf("\n--- Produtos Cadastrados ---\n");
     for (int i = 0; i < total; i++) {
+         printf("--------------------\n");
         // BUG: esqueceram de imprimir o ID e a quebra de linha está inadequada
-        printf("Nome: %s | Preco: R$ %.2f | Qtd: %d", lista[i].nome, lista[i].preco, lista[i].quantidade);
+        printf("\n id: %d | codigo_barras: %s Nome: %s | Preco: R$ %.2f | Qtd: %d \n",lista[i].id, lista[i].codigo_barras, lista[i].nome, lista[i].preco, lista[i].quantidade);
     }
 }
 
@@ -32,11 +33,13 @@ int main(void) {
     int total_produtos = 2;
 
     estoque[0].id = 1;
+    strcpy (estoque[0].codigo_barras,"7890001");
     strcpy(estoque[0].nome, "Caderno");
     estoque[0].preco = 15.50;
     estoque[0].quantidade = 10;
 
     estoque[1].id = 2;
+    strcpy (estoque[1].codigo_barras,"7890002");
     strcpy(estoque[1].nome, "Caneta");
     estoque[1].preco = 3.00;
     estoque[1].quantidade = 50;
@@ -46,9 +49,9 @@ int main(void) {
         exibir_menu();
         if (scanf("%d", &opcao) != 1) {
             break;
-        }
+        }4;
 
-        switch (opcao) {
+        switch(opcao) {
             case 1:
                 listar_produtos(estoque, total_produtos);
                 break;
